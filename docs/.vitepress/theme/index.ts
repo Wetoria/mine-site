1c668d8c0dae5b45202be86a36fd831859cbcfd5
Vip <vwetoria@gmail.com>
2024-08-10 12:00:32 +0800

    feat: 增加自动注册组件

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -4,6 +4,9 @@ import type { Theme } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
 import './style.css'
 
+// @ts-expect-error 类型 “ImportMeta” 上不存在属性 “glob”
+const modules = import.meta.glob("./../components/**/*.vue", { eager: true });
+
 export default {
   extends: DefaultTheme,
   Layout: () => {
@@ -13,5 +16,11 @@ export default {
   },
   enhanceApp({ app, router, siteData }) {
     // ...
+      for (const path in modules) {
+        const mod = modules[path];
+        const paths = path.split("/");
+        const moduleName = paths[paths.length - 1].replace(".vue", "");
+        app.component(moduleName, mod.default);
+      }
   }
 } satisfies Theme
